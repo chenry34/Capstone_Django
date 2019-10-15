ac26dfc6902e76504cd2ce70b0042b3cce366402
henryc35 <henryc35@myumanitoba.ca>
2019-10-15 11:40:04 -0500

    Create user table

--- Capstone_Django/models.py
+++ Capstone_Django/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True, unique=True, db_column='USER_ID')
+    user_name = models.CharField(max_length=255, db_column='USERNAME')
+    password = models.CharField(max_length=255, db_column='PASS')
+    created_date = models.DateField(db_column='CREATED_ON_DATETIME')
+
