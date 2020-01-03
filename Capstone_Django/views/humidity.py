6c5b9579b7905483e6a2ab2cca5d5851b0812857
henryc35 <henryc35@myumanitoba.ca>
2020-01-02 19:14:12 -0600

    Add humidity

--- Capstone_Django/views/humidity.py
+++ Capstone_Django/views/humidity.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
+from ..models import Humidity
+from django.http import JsonResponse
+import datetime
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_humidity(request):
-    return HttpResponse("Temperature is 20 degrees")
+    humidity = Humidity.objects.last()
+    return Response(humidity.value)
 
 
 def set_humidity(request):
+    humidity = Humidity(value=request.GET.get('humidity'), time_stamp=datetime.datetime.now())
+    humidity.save()
     return HttpResponse("success")
