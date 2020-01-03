2dbf28b971295c0cda5ac1a05fd37a175569302b
henryc35 <henryc35@myumanitoba.ca>
2020-01-02 18:53:42 -0600

    Write the view for window

--- Capstone_Django/views/window.py
+++ Capstone_Django/views/window.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
+from ..models import Window
+import datetime
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
-
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_window_status(request):
-    return HttpResponse("Temperature is 20 degrees")
+    window = Window.objects.last()
+    if window.value == 0:
+        return Response("Closed")
+    else:
+        return Response("Open")
 
 
 def set_window_status(request):
+    window = Window(value=request.GET.get('window'), time_stamp=datetime.datetime.now())
+    window.save()
     return HttpResponse("success")
