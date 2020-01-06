a540ea744f371fbb0c2b01342063f63eae5d5875
henryc35 <henryc35@myumanitoba.ca>
2020-01-06 12:26:27 -0600

    Add motion sensor

--- Capstone_Django/views/motion_sensor.py
+++ Capstone_Django/views/motion_sensor.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
+from ..models import Motion
+from django.http import JsonResponse
+import datetime
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_motion_status(request):
-    return HttpResponse("Temperature is 20 degrees")
+    motion = Motion.objects.last()
+    return Response(motion.value)
 
 
 def set_motion_status(request):
+    motion = Motion(value=request.GET.get('motion'), time_stamp=datetime.datetime.now())
+    motion.save()
     return HttpResponse("success")
+
