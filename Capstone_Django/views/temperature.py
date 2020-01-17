adc56d3e840be01496b8fe6fc4c707646df2f119
henryc35 <henryc35@myumanitoba.ca>
2020-01-17 11:38:07 -0600

    Add temperature

--- Capstone_Django/views/temperature.py
+++ Capstone_Django/views/temperature.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
+from ..models import Temperature
+from django.http import JsonResponse
+import datetime
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
-# Example of how temperature request will look
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_temperature(request):
-    return HttpResponse("Temperature is 20 degrees")
+    temp = Temperature.objects.last()
+    return Response(temp.value)
 
 
 def set_temperature(request):
+    temp = Temperature(value=request.GET.get('temperature'), time_stamp=datetime.datetime.now())
+    temp.save()
     return HttpResponse("success")
