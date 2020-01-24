e69b49b5ad17c43cb8cdb31c49f0efb95f729048
henryc35 <henryc35@myumanitoba.ca>
2020-01-24 13:57:26 -0600

    Return 24 hours worth of data

--- Capstone_Django/views/motion_sensor.py
+++ Capstone_Django/views/motion_sensor.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from ..models import Motion
 from django.http import JsonResponse
-import datetime
+from datetime import datetime, timedelta
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -10,12 +10,13 @@ from rest_framework.permissions import IsAuthenticated
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_motion_status(request):
-    motion = Motion.objects.last()
-    return Response(motion.value)
+    date = datetime.today() - timedelta(days=1)
+    motion = Motion.objects.filter(time_stamp__gte=date)
+    return Response(motion.values())
 
 
 def set_motion_status(request):
-    motion = Motion(value=request.GET.get('motion'), time_stamp=datetime.datetime.now())
+    motion = Motion(value=request.GET.get('motion'), time_stamp=datetime.now())
     motion.save()
     return HttpResponse("success")
 
